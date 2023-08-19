48f7c228479aaaea336df6acdbe40dc75ddf4c75
Nauman <naumanumer10@outlook.com>
2023-08-19 14:10:15 +0100

    Feature : Add a Employee

---
+++ script.js
@@ -0,0 +1,11 @@
+let addEmp=document.querySelector(".add-Emp");
+let modal=document.querySelector(".modal");
+let closeBtn=document.querySelector(".close-icon")
+addEmp.addEventListener("click",()=>{
+    modal.classList.add("active")
+
+})
+closeBtn.addEventListener("click",()=>{
+    modal.classList.remove("active")
+
+})
