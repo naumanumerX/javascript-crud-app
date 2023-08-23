669771ae9fe223c4e36e86a392d617119b504242
Nauman <naumanumer10@outlook.com>
2023-08-23 23:10:16 +0100

    Responsive Modal

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let addEmp=document.querySelector(".add-Emp");
 let modal=document.querySelector(".modal");
 let closeBtn=document.querySelector(".close-icon")
 addEmp.addEventListener("click",()=>{
+
     modal.classList.add("active")
 
 })
