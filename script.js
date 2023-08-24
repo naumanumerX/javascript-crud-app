4d9bd31652639b01c350d275ccdf25a37bcacd5d
Nauman <naumanumer10@outlook.com>
2023-08-24 22:47:14 +0100

    Storing Data To Local Storage

--- script.js
+++ script.js
@@ -10,3 +10,41 @@ closeBtn.addEventListener("click",()=>{
     modal.classList.remove("active")
 
 })
+// Global variables
+let regBtn=document.querySelector("#register-btn");
+let id=document.querySelector("#id");
+let fname=document.querySelector("#fname");
+let lname=document.querySelector("#lname");
+let ni=document.querySelector("#ni");
+let address=document.querySelector("#address");
+let email=document.querySelector("#email");
+let userInformation=[];
+let formRegistration=document.querySelector("#form-registration");
+
+//End of Global varaibales
+
+//Register Code
+
+regBtn.onclick=function(e){
+    e.preventDefault();
+    registrationData();
+    formRegistration.reset(' ');
+    closeBtn.click();
+
+}
+
+const registrationData= ()=>{
+    userInformation.push({
+        id:id.value,
+        fname:fname.value,
+        lname:lname.value,
+        ni:ni.value,
+        address:address.value,
+        email:email.value
+
+    })
+    console.log(userInformation);
+  const userDataString=JSON.stringify(userInformation);
+  console.log(userDataString);
+  localStorage.setItem("User Data",userDataString);
+}
